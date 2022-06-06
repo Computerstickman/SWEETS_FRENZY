7f88e5ae36efe0930005f9dcdb0f2f76c8efd3e5
Computerstickman <krystalc.paicos@gmail.com>
2022-06-06 16:35:16 -0700

    Comments for turnin

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+/*
+Stell Paicos - SWEETS FRENZY! - about two days
+
+Points-
+
+-60 in S tier, redesigned all artwork, UI and sound for a cute aesthetic
+-20 in Intermiediate for new spaceship type (the specialcake, one with stars)
+-20 in Intermediate for new weapon (tounge)
+-5 in Starting for randomizing movement (in this case spawn location for cakes)
+
+Most help was gotten from the phaser page itself/documentation :)
+
+*/
+
+
 let config = {
     type: Phaser.CANVAS,
     autoCenter: true,
